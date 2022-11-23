502411159929a40ae21d7797bbf990ff7de08752
Alexander Jansen <alexander.jansen@studmail.w-hs.de>
2022-11-23 20:40:36 +0100

    added OpenCV Flask socket stream from: https://nickhuber.ca/blog/python-opencv-camera-websockets

--- app.py
+++ app.py
@@ -1,12 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import base64
+
 from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+from engineio.payload import Payload
+
+from camera.camera import Camera
 
 app = Flask(__name__)
+Payload.max_decode_packets = 500
+socketio = SocketIO(app)
+
+camera = Camera()
+camera.start()
 
 
-@app.route('/')
-def hello_world():
+@app.route("/")
+def index():
+    """Video streaming home page."""
     return render_template("index.html")
 
 
-if __name__ == '__main__':
-    app.run()
+@socketio.on("request-frame", namespace="/camera-feed")
+def camera_frame_requested(message):
+    frame = camera.get_frame()
+    if frame is not None:
+        emit("new-frame", {
+            "base64": base64.b64encode(frame).decode("ascii")
+        })
+
+
+if __name__ == "__main__":
+    try:
+        socketio.run(app, host="0.0.0.0", port=8080)
+    except KeyboardInterrupt:
+        camera.stop()
---
+++ static/main.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const FPS = 62.5 // => ( 62.5; 50; 40; 32; 31.25; 25; 20; 16 )
+    const TBF = 1000/FPS //=> TBF = Time Between Frames
+
+    const socket = io.connect(`ws://${document.domain}:${location.port}/camera-feed`);
+    socket.on('new-frame', message => {
+        document.getElementById('camera-frame').setAttribute(
+            'src', `data:image/jpeg;base64,${message.base64}`
+        );
+    });
+    window.setInterval(() => {
+        socket.emit('request-frame', {});
+    }, TBF);
+});
